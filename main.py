48be8e7dd4800ed04e69569529ade100e681ba9a
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-09 00:05:08 -0500

    Final class

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-"atm" != "password"
+atm_password = input("banking:")
 
-if "atm" == "password":
+if atm_password == "banking":
+    
+    print("we are continuing")
+    
+elif atm_password == "yes":
     print("you may enter.")
+else:
+    print("please enter yes or no")  
 
-check_Balance  = 500
-print(f"Balance is 500.0", "Enter deposit amount 200.0, new Balance 700.0")
+user_input = ("would you like to [1] check_Balance [2] check_deposit [3] check_withdraw")
 
-check_withdraw = 200
-print(f"Balance is 500.0", "Enter withdraw amount 200, new Balance 300.0" )
+user_input = check_Balance  = 500
+print(f"Balance is 500.0")
 
-check_Exit_display = str
-print(f"Enter amount to withdraw: 200.0", "Your new Balance is 300.0 and user chooses to exit: Have a nice Day! ")
+user_input = check_deposit = 200
+print(f"Add 500 + 200", "new Balance 700" )
+
+user_input = check_withdraw = 200
+print(f"Enter amount to withdraw: 200.0", "Your new Balance is 300.0 ")
+
+user_input = check_withdraw = 200
+print(f"Your new Balance is 300", "user has chooses to exit", "Have a nice day!")
+        
+   
