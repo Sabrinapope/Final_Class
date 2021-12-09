17c651441bf6a48cdbd70bb22cfb22014280f3d7
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-08 23:06:43 -0500

    Final class !

---
+++ main.py
@@ -0,0 +1,13 @@
+"atm" != "password"
+
+if "atm" == "password":
+    print("you may enter.")
+
+check_Balance  = 500
+print(f"Balance is 500.0", "Enter deposit amount 200.0, new Balance 700.0")
+
+check_withdraw = 200
+print(f"Balance is 500.0", "Enter withdraw amount 200, new Balance 300.0" )
+
+check_Exit_display = str
+print(f"Enter amount to withdraw: 200.0", "Your new Balance is 300.0 and user chooses to exit: Have a nice Day! ")
